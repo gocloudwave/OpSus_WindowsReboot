a83ee8f1abc166fe5b736d4ab22115841a7bd92d
Dan Gill <dan.gill@dgillconsulting.com>
2022-06-02 16:09:45 -0500

    :tada: Add initial required files

---
+++ Install-PowerCLI.ps1
@@ -0,0 +1,149 @@
+#Requires -RunAsAdministrator
+<#
+.SYNOPSIS
+    A script to automate the install of VMware PowerCLI.
+.DESCRIPTION
+    A script to automate the install of VMware PowerCLI. Generally it is best
+    to call this script from another script to ensure PowerCLI commands are
+    available.
+.NOTES
+    File Name  : Install-PowerCLI.ps1
+    Author     : Dan Gill - dgill@gocloudwave.com
+.LINK
+    https://virtuallysober.com/2017/06/01/automatically-updating-to-powercli-6-5-1/
+.LINK
+    https://docs.microsoft.com/en-us/officeonlineserver/enable-tls-1-1-and-tls-1-2-support-in-office-online-server#enable-strong-cryptography-in-net-framework-45-or-higher
+.INPUTS
+    None. You cannot pipe objects to Install-PowerCLI.ps1.
+.OUTPUTS
+    None. Install-PowerCLI.ps1 does not generate output.
+.EXAMPLE
+    PS> .\Install-PowerCLI.ps1
+.EXAMPLE
+    PS> Invoke-Expression -Command "Install-PowerCLI.ps1"
+#>
+
+# Specify a directory to download and install the new PowerCLI module to for future offline access
+$PSModulePath = $Env:SystemDrive + '\PowerCLIModule\'
+
+#######################
+# Testing if TLS 1.2 or above is enabled
+#######################
+if ([Net.ServicePointManager]::SecurityProtocol -notlike '*Tls1[23]*') {
+  #######################
+  # Set Strong Cryptography - https://docs.microsoft.com/en-us/officeonlineserver/enable-tls-1-1-and-tls-1-2-support-in-office-online-server#enable-strong-cryptography-in-net-framework-45-or-higher
+  #######################
+  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force -Confirm:$false
+  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force -Confirm:$false
+  #######################
+  # Disable TLS 1.0 and 1.1
+  #######################
+  if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client') {
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+  }
+  if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server') {
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+  }
+  if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client') {
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+  }
+  if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server') {
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+  }
+  #######################
+  # Exit and ask user to re-run the script
+  #######################
+  Write-Warning 'This system did not have TLS 1.2 or above enabled. Please re-run the script so that the changes to enable TLS 1.2 and above take effect.'
+  Exit
+}
+
+#######################
+# Testing if PS Module path exists, if not creating it
+#######################
+$PSModulePathTest = Test-Path $PSModulePath
+if ($PSModulePathTest -eq $False) {
+  New-Item -ItemType Directory -Force -Path $PSModulePath
+}
+#######################
+# Checking to see if PowerCLI is installed in Program Files, takes 5-30 seconds
+#######################
+Write-Progress -Id 1 -Activity 'Checking for PowerCLI' -CurrentOperation 'Checking if PowerCLI is installed in Program Files, wait 5-30 seconds'
+$PowerCLIInstalled = Get-WmiObject -Class Win32_Product -Filter "Name='VMware vSphere PowerCLI'"
+#######################
+# If PowerCLI is installed then removing it, so we can run from the module instead
+#######################
+if ($PowerCLIInstalled) {
+  Write-Progress -Id 2 -ParentId 1 -Activity 'PowerCLI in Program Files' -CurrentOperation 'Uninstalling to allow for new PowerCLI module'
+  # Uninstalling PS module
+  $PowerCLIUninstall = $PowerCLIInstalled.Uninstall()
+  # Checking return value for success
+  $PowerCLIUninstallValue = $PowerCLIUninstall.ReturnValue
+  if ($PowerCLIUninstallValue -ne 0) {
+    Write-Error -Message 'Uninstall Of PowerCLI Failed - Most likely due to not running as administrator' -Category PermissionDenied
+  }
+  # Finished uninstall
+  Write-Progress -Id 2 -ParentId 1 -Activity 'PowerCLI in Program Files' -Completed
+}
+Write-Progress -Id 1 -Activity 'Checking for PowerCLI' -Completed
+#######################
+# Checking to see if the NuGet Package Provider is already installed
+#######################
+$NuGetPackageProviderCheck = Get-PackageProvider -Name 'NuGet' -ListAvailable
+#######################
+# If NuGet Provider is not installed, nothing found, then running install...
+#######################
+if (!$NuGetPackageProviderCheck) {
+  Write-Progress -Id 1 -Activity 'NuGet Package Provider' -CurrentOperation 'Not Found - Installing'
+  # Trusting PS Gallery to remove prompt on install
+  Set-PackageSource -Name 'PSGallery' -Trusted
+  # Not installed, finding module online
+  Find-PackageProvider -Name 'NuGet' -AllVersions
+  # Installing module
+  Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+  Write-Progress -Id 1 -Activity 'NuGet Package Provider' -Completed
+}
+#######################
+# Checking to see if the PowerCLI module is already installed
+#######################
+$PowerCLIModuleCheck = Get-Module -ListAvailable -Name 'VMware.PowerCLI'
+#######################
+# If PowerCLI module is not installed, nothing found, then running install...
+#######################
+if (!$PowerCLIModuleCheck) {
+  Write-Progress -Id 1 -Activity 'PowerCLI Module' -CurrentOperation 'Not Found - Installing'
+  # Trusting PS Gallery to remove prompt on install
+  Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+  # Not installed, finding module online
+  Find-Module -Name 'VMware.PowerCLI'
+  # Installing module
+  Install-Module -Name 'VMware.PowerCLI' -Confirm:$false -AllowClobber -Force
+  # If running this is a repeat demo/test, you can uninstall the module using the below:
+  # Uninstall-Module -Name VMware.PowerCLI -Confirm:$false
+  Write-Progress -Id 1 -Activity 'PowerCLI Module' -Completed
+}
+#######################
+# Testing import of PowerCLI module
+#######################
+Write-Progress -Id 1 -Activity 'PowerCLI Module' -CurrentOperation 'Importing'
+$null = Import-Module -Name 'VMware.PowerCLI'
+Write-Progress -Id 1 -Activity 'PowerCLI Module' -Completed
+Try {
+  $null = Get-VICommand
+  $PowerCLIImportTest = $True
+} Catch {
+  $PowerCLIImportTest = $False
+} Finally {
+  #######################
+  # Outputting result
+  #######################
+  if ($PowerCLIImportTest) {
+    Write-Information 'New PowerCLI Module Successfully Installed'
+  } else {
+    Write-Error -Message "Something went wrong! Maybe you, maybe me. Does this computer have internet access and did you run as administrator?`r`nTry installing PowerCLI in offline mode (Procedure 3): https://tinyurl.com/VMware-PowerCLI"
+    Exit 22
+  }
+}
---
+++ User-Prompts.ps1
@@ -0,0 +1,135 @@
+<#
+.SYNOPSIS
+    Header file to create user dialog box.
+.DESCRIPTION
+    A script to create a user dialog box. This script always runs from another
+    script's call.
+.NOTES
+    File Name  : User-Prompts.ps1
+    Author     : Dan Gill - dgill@gocloudwave.com
+.INPUTS
+    None. You cannot pipe objects to User-Prompts.ps1.
+.OUTPUTS
+    None. User-Prompts.ps1 does not generate output.
+.EXAMPLE
+    PS> .\User-Prompts.ps1
+#>
+
+<#
+.SYNOPSIS
+    Create user dialog box with single list item selection.
+.DESCRIPTION
+    Create user dialog box with single list item selection.
+    Takes a Title, Message, List, and optional size parameters.
+.PARAMETER Title
+    Specifies the name of the dialog box.
+.PARAMETER Prompt
+    Specifies the message to prompt the user.
+.PARAMETER Values
+    Specifies an array to list for single selection.
+.PARAMETER Width
+    Specifies the width of the dialog box. Defaults to 300 pixels.
+.PARAMETER Height
+    Specifies the height of the dialog box. Defaults to 200 pixels.
+.LINK
+    https://docs.microsoft.com/en-us/powershell/scripting/samples/selecting-items-from-a-list-box?view=powershell-7.2
+.INPUTS
+    None. You cannot pipe objects to myDialogBox.
+.OUTPUTS
+    System.String. myDialogBox returns a string with the selected item.
+.EXAMPLE
+    PS> $SelectedItem = myDialogBox -Title 'Title' -Prompt 'Message' `
+    -Values $SelectionList
+.EXAMPLE
+    PS> $SelectedItem = myDialogBox -Title 'Title' -Prompt 'Message' `
+    -Values $SelectionList -Width 400
+.EXAMPLE
+    PS> $SelectedItem = myDialogBox -Title 'Title' -Prompt 'Message' `
+    -Values $SelectionList -Height 400
+.EXAMPLE
+    PS> $SelectedItem = myDialogBox -Title 'Title' -Prompt 'Message' `
+    -Values $SelectionList -Width 400 -Height 400
+#>
+function myDialogBox {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string[]]$Title,
+        [Parameter(Mandatory)]
+        [string[]]$Prompt,
+        [Parameter(Mandatory)]
+        [string[]]$Values,
+        [Parameter(Mandatory = $false)]
+        [Int]$Width = 300,
+        [Parameter(Mandatory = $false)]
+        [Int]$Height = 200
+    )
+
+    begin {
+        Add-Type -AssemblyName System.Windows.Forms
+        Add-Type -AssemblyName System.Drawing
+
+        $buttonWidth = 75
+        $buttonHeight = 23
+        $buttonTopEdge = $Height - 80
+        $OKbuttonLeftEdge = ( $Width / 2 ) - $buttonWidth
+    }
+
+    process {
+        $form = New-Object System.Windows.Forms.Form
+        $form.Text = $Title
+        $form.Size = New-Object System.Drawing.Size($Width, $Height)
+        $form.StartPosition = 'CenterScreen'
+    
+        $okButton = New-Object System.Windows.Forms.Button
+        $okButton.Location = New-Object System.Drawing.Point($OKbuttonLeftEdge, $buttonTopEdge)
+        $okButton.Size = New-Object System.Drawing.Size($buttonWidth, $buttonHeight)
+        $okButton.Text = 'OK'
+        $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+        $form.AcceptButton = $okButton
+        $form.Controls.Add($okButton)
+    
+        $cancelButton = New-Object System.Windows.Forms.Button
+        $cancelButton.Location = New-Object System.Drawing.Point( ( $Width / 2 ), $buttonTopEdge)
+        $cancelButton.Size = New-Object System.Drawing.Size($buttonWidth, $buttonHeight)
+        $cancelButton.Text = 'Cancel'
+        $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+        $form.CancelButton = $cancelButton
+        $form.Controls.Add($cancelButton)
+    
+        $label = New-Object System.Windows.Forms.Label
+        $label.Location = New-Object System.Drawing.Point(10, 20)
+        $label.Size = New-Object System.Drawing.Size(($Width - 20), 20)
+        $label.Text = $Prompt
+        $form.Controls.Add($label)
+    
+        $listBox = New-Object System.Windows.Forms.ListBox
+        $listBox.Location = New-Object System.Drawing.Point(10, 40)
+        $listBox.Size = New-Object System.Drawing.Size(($Width - 40), 20)
+        $listBox.Height = $Height - 120
+    
+        foreach ($value in $Values) { [void] $listBox.Items.Add($value) }
+    
+        $form.Controls.Add($listBox)
+    
+        $form.Topmost = $true
+    
+        $form.Add_Shown({ $listBox.Select() })
+    
+        $result = $form.ShowDialog()
+    
+        if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+            $SelectedItem = $listBox.SelectedItem
+            
+        } else {
+            # Return null, determine what happens when user cancels in calling script
+            $SelectedItem = $null
+        }
+    }
+
+    end {
+        return $SelectedItem
+    }
+    
+}
