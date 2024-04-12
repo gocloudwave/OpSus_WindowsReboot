c80f79bb1c210d462d8c165cea0ac194fbbcc0d3
Dan Gill <dan.gill@dgillconsulting.com>
2024-04-12 15:30:29 -0500

    :sparkles: Add multi-select dialog box

--- User-Prompts.ps1
+++ User-Prompts.ps1
@@ -240,3 +240,87 @@ function Get-FileName {
     }
 
 }
+
+function Select-MultiOptionDialogBox {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string[]]$Title,
+        [Parameter(Mandatory)]
+        [string[]]$Prompt,
+        [Parameter(Mandatory)]
+        [string[]]$Values,
+        [Parameter(Mandatory = $false)]
+        [Int]$Width = 300,
+        [Parameter(Mandatory = $false)]
+        [Int]$Height = 200
+    )
+
+    begin {
+        Add-Type -AssemblyName System.Windows.Forms
+        Add-Type -AssemblyName System.Drawing
+
+        $buttonWidth = 75
+        $buttonHeight = 23
+        $buttonTopEdge = $Height - 80
+        $OKbuttonLeftEdge = ( $Width / 2 ) - $buttonWidth
+    }
+
+    process {
+        $form = New-Object System.Windows.Forms.Form
+        $form.Text = $Title
+        $form.Size = New-Object System.Drawing.Size($Width, $Height)
+        $form.StartPosition = 'CenterScreen'
+
+        $okButton = New-Object System.Windows.Forms.Button
+        $okButton.Location = New-Object System.Drawing.Point($OKbuttonLeftEdge, $buttonTopEdge)
+        $okButton.Size = New-Object System.Drawing.Size($buttonWidth, $buttonHeight)
+        $okButton.Text = 'OK'
+        $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+        $form.AcceptButton = $okButton
+        $form.Controls.Add($okButton)
+
+        $cancelButton = New-Object System.Windows.Forms.Button
+        $cancelButton.Location = New-Object System.Drawing.Point( ( $Width / 2 ), $buttonTopEdge)
+        $cancelButton.Size = New-Object System.Drawing.Size($buttonWidth, $buttonHeight)
+        $cancelButton.Text = 'Cancel'
+        $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+        $form.CancelButton = $cancelButton
+        $form.Controls.Add($cancelButton)
+
+        $label = New-Object System.Windows.Forms.Label
+        $label.Location = New-Object System.Drawing.Point(10, 20)
+        $label.Size = New-Object System.Drawing.Size(($Width - 20), 20)
+        $label.Text = $Prompt
+        $form.Controls.Add($label)
+
+        $listBox = New-Object System.Windows.Forms.ListBox
+        $listBox.Location = New-Object System.Drawing.Point(10, 40)
+        $listBox.Size = New-Object System.Drawing.Size(($Width - 40), 20)
+        $listBox.Height = $Height - 120
+
+        $listBox.SelectionMode = 'MultiExtended'
+
+        foreach ($value in $Values) { [void] $listBox.Items.Add($value) }
+
+        $form.Controls.Add($listBox)
+
+        $form.Topmost = $true
+
+        $result = $form.ShowDialog()
+
+        if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+            $SelectedItems = $listBox.SelectedItems
+
+        } else {
+            # Return null, determine what happens when user cancels in calling script
+            $SelectedItems = $null
+        }
+    }
+
+    end {
+        return $SelectedItems
+    }
+
+
+}
